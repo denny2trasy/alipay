248f38f5e6a178833c1b6cb01d77deb0803049b8
denny2trasy <denny2trasy@gmail.com>
2015-10-06 12:55:10 +0800

    sign

--- lib/alipay/open/service.rb
+++ lib/alipay/open/service.rb
@@ -22,7 +22,9 @@ module Alipay
           'notify_url'    => options['notify_url']
         }.merge('biz_content' => params)
 
-        sign = Alipay::Open::Sign.generate(full_params)
+        string_params = Utils.stringify_keys_with_hash_value(full_params)
+
+        sign = rsa_sign(string_params)
 
         puts "+++++ Sign = #{sign}"
 
@@ -71,6 +73,27 @@ module Alipay
 
       def self.post_params(biz_content) 
         "biz_content=#{CGI.escape(biz_content.to_json.to_s)}"
+      end
+
+      def self.rsa_sign for_sign_string
+
+        #读取私钥文件
+        rsa_private_key_file = File.read(Alipay.rsa_private_key_file)
+        #转换为openssl密钥
+        openssl_key = OpenSSL::PKey::RSA.new rsa_private_key_file
+        #使用openssl方法进行sha1签名digest(不能用sha256)
+        puts " ++++ Openssl Key = #{openssl_key}"
+        digest = OpenSSL::Digest::SHA1.new
+        signature = openssl_key.sign digest, for_sign_string
+
+        puts " ++++ Digest = #{digest}"
+        puts " ++++ Signature no encode = #{signature}"
+
+        #base64编码
+        signature = Base64.encode64(signature)
+
+        puts " ++++ Signature encoded = #{signature}"
+        return signature.gsub("\n","")
       end
 
     end
