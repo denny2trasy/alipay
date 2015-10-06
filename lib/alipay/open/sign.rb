40b4964a42b119b4d9c5159b80dbebd85abc70b8
denny2trasy <denny2trasy@gmail.com>
2015-10-06 12:25:48 +0800

    sign

--- lib/alipay/open/sign.rb
+++ lib/alipay/open/sign.rb
@@ -12,21 +12,32 @@ module Alipay
         rsa_private_key_file = File.read(Alipay.rsa_private_key_file)
         #转换为openssl密钥
         openssl_key = OpenSSL::PKey::RSA.new rsa_private_key_file
-
         puts " +++++ Openssl Key = #{openssl_key}"
-        #使用openssl方法进行sha1签名digest(不能用sha256)
-        digest = OpenSSL::Digest::SHA1.new
-        signature = openssl_key.sign digest, for_sign_string
 
-        puts " ++++ Digest = #{digest}"
+        rsa = OpenSSL::PKey::RSA.new(openssl_key)
+        signature = rsa.sign 'sha1', for_sign_string
+
         puts " ++++ Signature no encode = #{signature}"
 
-        #base64编码
-        signature = Base64.encode64(signature)
+        signature = Base64.strict_encode64(signature)
 
         puts " ++++ Signature encoded = #{signature}"
-
+        
         return signature.gsub("\n","")
+        
+        # #使用openssl方法进行sha1签名digest(不能用sha256)
+        # digest = OpenSSL::Digest::SHA1.new
+        # signature = openssl_key.sign digest, for_sign_string
+
+        # puts " ++++ Digest = #{digest}"
+        # puts " ++++ Signature no encode = #{signature}"
+
+        # #base64编码
+        # signature = Base64.encode64(signature)
+
+        # puts " ++++ Signature encoded = #{signature}"
+
+        # return signature.gsub("\n","")
       end
 
     end
